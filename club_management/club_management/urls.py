67eabc9b4fd25e5713544cf9c66e7db34cd3ca63
UNCROOS <any492266@gmail.com>
2024-12-21 22:35:55 +0900

    url

--- club_management/club_management/urls.py
+++ club_management/club_management/urls.py
@@ -1,11 +1,22 @@
-from django.contrib.auth.views import LoginView, LogoutView
+"""
+URL configuration for club_management project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('', views.main_page, name='main'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('todo/', views.todo_page, name='todo'),
-    path('attendance/', views.attendance_page, name='attendance'),
+    path('admin/', admin.site.urls),
 ]
