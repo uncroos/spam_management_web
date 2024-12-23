b9c4cb61c772f3e5563452b710044771a3c00546
UNCROOS <any492266@gmail.com>
2024-12-23 17:02:35 +0900

    기초 설치

--- club_management/club/admin.py
+++ club_management/club/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Todo, Attendance
 
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'first_name', 'phone_number', 'is_teacher')
-    fieldsets = UserAdmin.fieldsets + (
-        ('추가 정보', {'fields': ('phone_number', 'is_teacher')}),
-    )
-
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'is_completed', 'created_at')
-    list_filter = ('user', 'is_completed')
-
-class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date')
-    list_filter = ('user', 'date')
-
-admin.site.register(User, CustomUserAdmin)
-admin.site.register(Todo, TodoAdmin)
-admin.site.register(Attendance, AttendanceAdmin)
+# Register your models here.
